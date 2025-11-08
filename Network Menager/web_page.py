ec8464aa8e410fdadc23504044a1a39e29bfc870
Szymon Złocki <szymonzlocki2001@gmail.com>
2025-11-08 21:36:19 +0100

    podstawowy template

--- Network Menager/web_page.py
+++ Network Menager/web_page.py
@@ -1,18 +1,10 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Welcome to the Home Page!"
-
-@app.route("/<name>")
-def page1(name):
-    return f"Hello, {name}! This is Page 1."
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for('home'))
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run() 
+    app.run(debug=True) 

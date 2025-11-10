1fa65b40adad787a44ebe4e9566c2117a43c2aff
Szymon Złocki <szymonzlocki2001@gmail.com>
2025-11-10 12:49:23 +0100

    laczenie przez ssh / dashboard/ menu wyboru stanu sieci

---
+++ Network Menager/run.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, Response, redirect,url_for,session
+from ssh_utils import connect_router, exec_ssh_command
+
+app = Flask(__name__)
+app.secret_key = "supersecretkey"
+
+@app.route("/")
+def index():
+    # strona z formularzem
+    return render_template("index.html")
+
+@app.route("/connect", methods=["POST"])
+def connect():
+    ip = request.form.get("ip")
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    # wykonanie polecenia przez SSH
+    success, message = connect_router(ip, username, password)
+
+    if success:
+        session['ip'] = ip
+        session['username'] = username
+        session['password'] = password
+        return redirect(url_for('dashboard'))
+    else:
+        return render_template("index.html", error=message)
+    
+@app.route("/dashboard")
+def dashboard():
+    ip = session.get('ip')
+    username = session.get('username')
+    return render_template("dashboard.html", ip=ip, username=username)
+
+@app.route("/lan")
+def lan():
+
+    ip = session.get('ip')
+    username = session.get('username')
+    password = session.get('password')
+    lan_info = exec_ssh_command(ip, username, password, "ifconfig br-lan")
+
+    return render_template("lan.html", lan_info=lan_info)
+
+@app.route("/wireless")
+def wireless():
+
+    ip = session.get("ip")
+    username = session.get("username")
+    password = session.get("password")
+    wifi_info = exec_ssh_command(ip, username, password, "iwinfo")
+    return render_template("wireless.html", wifi_info=wifi_info)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Network Menager/ssh_utils.py
@@ -0,0 +1,26 @@
+import paramiko
+
+def connect_router(ip, username, password):
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(ip, username=username, password=password, port=22, timeout=10)
+        ssh.close()
+        # ZWRACAMY DOKŁADNIE DWA ELEMENTY!
+        return True, f"✅ Połączono pomyślnie z routerem {ip}"
+    except Exception as e:
+        # Również DWA ELEMENTY (False + wiadomość)
+        return False, f"❌ Błąd połączenia: {e}"
+
+def exec_ssh_command(ip, username, password, command):
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(ip, username=username, password=password, port=22, timeout=10)
+        
+        stdin, stdout, stderr = ssh.exec_command(command)
+        output = stdout.read().decode(errors="ignore")
+        ssh.close()
+        return output or "Brak danych z routera."
+    except Exception as e:
+        return f"Błąd podczas wykonywania komendy: {e}"
